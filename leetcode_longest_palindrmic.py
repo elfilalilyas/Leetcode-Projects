0f3b8923af8eeafaf1cf534bd06339057c89bd3f
Elfilali ilyas <157057721+elfilalilyas@users.noreply.github.com>
2025-02-13 21:30:49 +0100

    Update leetcode_longest_palindrmic.py

--- leetcode_longest_palindrmic.py
+++ leetcode_longest_palindrmic.py
@@ -1,69 +1,4 @@
-import time
-# def fun(string):
-#     k = len(string)
-#     while k > 0:
-#         L = 0
-#         while True:
-#             R = L + k
-#             if R == len(string) + 1:
-#                 break
-#             s = string[L:R]
-#             print(s)
-#             rev_s = list(s)
-#             rev_s.reverse()
-#             if s == ''.join(rev_s):
-#                 return s
-#             L += 1
-#         k -= 1
-# def fun(s):
-#     l = 0
-#     r = len(s) - 1
-#     st = ''
-#     while l < len(s):
-#         print('--', st)
-#         print(l, r)
-#         if s[l] == s[r]:
-#             ss = s[l:r+1]
-#             ss_list = list(ss)
-#             if ss_list == ss_list[::-1]:
-#                 print(ss)
-#                 if len(ss) > len(st):
-#                     st = ss
-#                 l += 1
-#                 r = len(s) - 1
-#                 continue
-#         if l == r:
-#             l += 1
-#             r = len(s) - 1
-#             if l > r:
-#                 break
-#         r -= 1
-#     return st
-###-----
-# else:
-# st = s[c]
-# while l >= 0 and r < len(s):
-#     if s[l] != s[r]:
-#         break
-#     st = s[l] + st + s[r]
-#     r += 1
-#     l -= 1
-# if len(st) > len(palindorm):
-#     palindorm = st
-# if s[c] == s[c + 1]:
-#     st = s[c] + s[c + 1]
-#     l = c - 1
-#     r = c + 2
-#     while l >= 0 and r < len(s):
-#         if s[l] != s[r]:
-#             break
-#         st = s[l] + st + s[r]
-#         r += 1
-#         l -= 1
-#     if len(st) > len(palindorm):
-#         palindorm = st
-
-## my method
+# method 1
 def fun(s):
     palindorm = ''
     if len(s) == 1:
@@ -71,7 +6,7 @@ def fun(s):
     for c in range(len(s) - 1):
         l, r = c - 1, c + 1
         st = s[c]
-        # this for odd palindromic substring
+        # for odd palindromic substring
         while l >= 0 and r < len(s):
             if s[l] != s[r]:
                 break
@@ -80,7 +15,7 @@ def fun(s):
             l -= 1
         if len(st) > len(palindorm):
             palindorm = st
-        # this for even palindromic substring
+        # for even palindromic substring
         if s[c] == s[c+1]:
             st = s[c] + s[c + 1]
             l, r = c - 1, c + 2
@@ -95,7 +30,7 @@ def fun(s):
 
     return palindorm
 
-## the best method
+# method 2
 def theLongestSubsS(s):
     palin = ''
     if len(s) <= 1:
